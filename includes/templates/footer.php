6ebe8d330281e3730e279c24acbde0a0784bc200
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2023-09-14 20:24:48 -0600

    Add information to footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -7,7 +7,7 @@
                 <a href="contacto.php">Contacto</a>
             </nav>
         </div>
-        <p class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy;</p>
+        <p class="copyright">Desarrollado por Daniel Jiménez | Todos los Derechos Reservados <?php echo date('Y'); ?> &copy;</p>
     </footer>
     <script src="/build/js/bundle.min.js"></script>
 </body>
